cba55635dec432a1738892882f9fe748348d99ed
duyluan38ht <70797139+duyluan38ht@users.noreply.github.com>
2020-12-08 15:15:02 +0700

    Add files via upload

---
+++ case_model2/CASE_STADY_MODEL2/index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])){
+    header('location:login.php');
+}
+?>
+<?php
+
+require __DIR__."/vendor/autoload.php";
+$controllerRoom = new \App\controller\ControllerRoom();
+$controllerOrder = new \App\controller\ControllerOder();
+$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : NULL;
+switch ($page) {
+    case 'bookRoom':
+        $controllerRoom->bookRoom();
+        break;
+    case 'checkRoom':
+        $controllerOrder->checkRoom();
+        break;
+    case 'delete':
+        $controllerRoom->delete();
+        break;
+    default:
+        $controllerRoom->showRoom();
+        break;
+}
+
+
+?>
